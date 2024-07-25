0c760f2898e5386c418a35dadfa5f775545c4fac
Alexander Nalobin <m_troc@mail.ru>
2024-07-25 06:02:40 +0300

    Some code-draft

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	storage := NewInMemoryStorage()
+
+	n := Node{ID: "1", Value: "first"}
+	storage.Add(n)
+
+	node, exists := storage.Get("1")
+
+	if exists {
+		fmt.Printf("Node exists: %v\n", node)
+	} else {
+		fmt.Printf("Node does not exist\n")
+	}
+
+	storage.Delete("1")
 }
